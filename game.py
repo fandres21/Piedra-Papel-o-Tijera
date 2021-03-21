56ff9d5fc078649807dbc965618ec07f1026adab
MarioCastillo <mariocastillosan@gmail.com>
2021-03-21 18:44:21 -0300

    Cambio en el sistema comparativo a uno numerico, condicion de puntos para finalizar el juego y saber quien lleva el liderato para futuros usos

--- game.py
+++ game.py
@@ -3,41 +3,47 @@ from time import sleep
 from random import randint
 
 array = ["piedra","papel","tijera"]
-
 contUser = 0
 contPc = 0
-while True:
+condicion=True
+while condicion:
     os.system("cls")
     print("JUEGO DE PIEDRA, PAPEL O TIJERA")
     pc = array[randint(0,len(array)-1)]
+    pci=array.index(pc)
     print("Tu tienes: {} puntos y la maquina tiene {} puntos ".format(contUser,contPc))
     rsp = input('Ingresa piedra,papel o tijera:\n >> ').lower()
-    if rsp not in  array:
+    rspi=array.index(rsp)
+    if rsp not in array:
         print("Ingresa una de las opciones validas para jugar")
         sleep(1)
-        os.system('cls')        
+        os.system('cls')
+    elif pci >rspi:
+        if pci ==2 and rspi==0:
+            print("Ganaste, {} gana a {}".format(rsp, pc))
+            contUser += 1
+        else:
+            print("Perdiste, {} gana a {}".format(pc,rsp))
+            contPc += 1
+    elif pci == rspi:
+        print("Es un empate de dos {}s".format(pc))
+    elif pci<rspi:
+        if pci == 0 and rspi == 2:
+            print("Perdiste, {} gana a {}".format(pc, rsp))
+            contPc += 1
+        else:
+            print("Ganaste, {} gana a {}".format(rsp, pc))
+            contUser += 1
+    sleep(2)
+    if contPc>contUser:
+        contLider="La Maquina"
     else:
-        if rsp == pc :
-            print("Es un empate")
-        elif rsp == 'piedra' and pc == 'papel':
-            contPc += 1
-            print("Perdiste, papel gana a piedra")
-        elif rsp == 'papel' and pc == 'tijera':
-            contPc += 1
-            print("Perdiste, tijera corta papel")
-        elif rsp == "tijera" and pc == "piedra":
-            contPc += 1
-            print("Perdiste, Piedra rompe tijera")
-        elif rsp == "papel" and pc == "piedra":
-            contUser += 1
-            print("Ganaste, papel envuelve piedra")
-        elif rsp == "tijera" and pc == "papel":
-            contUser += 1
-            print("Ganaste, Tijera corta Papel")
-        elif rsp == "piedra" and pc == "tijera":
-            contUser += 1
-            print("Ganaste, Piedra Rompe Tijera")
-        sleep(2) 
+        contLider="El Jugador"
+    if(contPc>3 or contUser>3):
+        print("El juego a terminado, el ganador es {}".format(contLider))
+        condicion=False
+
+
 
 
 
