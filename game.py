7f273987ec70813ec688ba9113908aa4636ca912
MarioCastillo <mariocastillosan@gmail.com>
2021-03-23 00:44:59 -0300

    JueGo V2.0

--- game.py
+++ game.py
@@ -7,8 +7,6 @@ from dearpygui.core import *
 from dearpygui.simple import *
 from PIL import Image
 array = ["piedra", "papel", "tijera"]
-contUser = 0
-contPc = 0
 condicion = True
 clear= lambda :os.system('cls')
 #Imagenes para opciones
@@ -24,77 +22,59 @@ def whoWin(contPc, contUser):
     if(contPc > 3 or contUser > 3):
         print("El juego a terminado, el ganador es {}".format(contLider))
         condicion = False
+        configure_item(".",enabled=False)
     return condicion
 
 
 def presentacion():
     clear()
-    #print("JUEGO DE PIEDRA, PAPEL O TIJERA")
     pc = array[randint(0, len(array)-1)]
     pci = array.index(pc)
     return [pc, pci]
 
-
-# if __name__ == "__main__":
-#     while condicion:
-#         presentacion()
-#         pc,pci = presentacion()
-#         print("Tu tienes: {} puntos y la maquina tiene {} puntos ".format(contUser,contPc))
-#         rsp = input('Ingresa piedra,papel o tijera:\n >> ').lower()
-#         if rsp not in array:
-#             print("Ingresa una de las opciones validas para jugar")
-#             sleep(1)
-#             clear()
-#         else:
-#             rspi = array.index(rsp)
-#             if pci >rspi:
-#                 if pci ==2 and rspi==0:
-#                     print("Ganaste, {} gana a {}".format(rsp, pc))
-#                     contUser += 1
-#                 else:
-#                     print("Perdiste, {} gana a {}".format(pc,rsp))
-#                     contPc += 1
-#             elif pci == rspi:
-#                 print("Es un empate de dos {}s".format(pc))
-#             elif pci<rspi:
-#                 if pci == 0 and rspi == 2:
-#                     print("Perdiste, {} gana a {}".format(pc, rsp))
-#                     contPc += 1
-#                 else:
-#                     print("Ganaste, {} gana a {}".format(rsp, pc))
-#                     contUser += 1
-#         sleep(2)
-#         condicion = whoWin(contPc,contUser)
-
 ###########################################
 #####INTERFAZ VISUAL #####################
 ##########################################
-def obtenerSelectBox(Sender):
-    # print para comprobar resultado en Str
-    #print(array[core.get_value(Sender)])
-    return str(array[core.get_value(Sender)])
+contUser=0
+contPc=0
+def MainLoop(Sender):
+    global contUser
+    global contPc
+    presentacion()
+    pc, pci = presentacion()
+    print("Tu tienes: {} puntos y la maquina tiene {} puntos ".format(contUser, contPc))
+    valueRetorn=str(array[core.get_value(Sender)])
+    valueRetorni = array.index(valueRetorn)
+    if pci>valueRetorni:
+        if pci == 2 and valueRetorni == 0:
+            print("Ganaste, {} gana a {}".format(valueRetorn, pc))
+            contUser+=1
+        else:
+            print("Perdiste, {} gana a {}".format(pc, valueRetorn))
+            contPc += 1
+    elif pci == valueRetorni:
+        print("Es un empate de dos {}s".format(pc))
+    elif pci<valueRetorni:
+        if pci == 0 and valueRetorni == 2:
+            print("Perdiste, {} gana a {}".format(pc, valueRetorn))
+            contPc += 1
+        else:
+            print("Ganaste, {} gana a {}".format(valueRetorn, pc))
+            contUser += 1
+    set_value("Tu tienes: 0 puntos y la maquina tiene 0 puntos","Tu tienes: {} puntos y la maquina tiene {} puntos".format(contUser, contPc))
+    condicion = whoWin(contPc,contUser)
 
+with simple.window("PPT"):
 
-with simple.window("JUEGO DE PIEDRA, PAPEL O TIJERA"):
     core.set_main_window_size(500, 500)
     pc, pci = presentacion()
-    core.add_text(
-        "Tu tienes: {} puntos y la maquina tiene {} puntos ".format(contUser, contPc))
-
+    core.add_text(name="Tu tienes: 0 puntos y la maquina tiene 0 puntos",
+        tip="Contenedor Puntos")
     opcionElegida = core.add_listbox(
-        name="",
+        name=".",
         items=array,
         num_items=3,
         default_value = 0,
-        callback=obtenerSelectBox,
-        label = "aasdsads"
-        
+        callback=MainLoop,
         )
-        
-    print(opcionElegida)
-
-    entrada = core.add_input_text(
-        "string", default_value="Ingresa piedra,papel o tijera:\n >> ".lower())
-    # get_value(entrada)
-
-core.start_dearpygui(primary_window="JUEGO DE PIEDRA, PAPEL O TIJERA")
+core.start_dearpygui(primary_window="PPT")
